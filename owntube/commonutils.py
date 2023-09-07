7a8fd22fb221cb547c2a3e8bdfd2f66ccbbf0827
Nathan Campos <nathan@innoveworkshop.com>
2023-09-07 16:20:06 +0100

    Added a helper function to quickly load our configuration file
    Since we'll be dealing with databases and "private" information related
    to the database credentials we'll use a simple YAML configuration file
    to store this information and any extra configuration that we might need
    in the future.

--- owntube/commonutils.py
+++ owntube/commonutils.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 """A collection of common utility functions to help us out."""
 
+from os.path import abspath, dirname
+
 import requests
+import yaml
+
+def read_config(path = None):
+    """Reads the default configuration file or a specific one."""
+    # Use the default path if one wasn't provided.
+    if path is None:
+        path = dirname(dirname(abspath(__file__))) + '/config.yml'
+
+    # Check if we have already cached the configuration file.
+    if not hasattr(read_config, 'config'):
+        with open(path, 'r') as fh:
+            read_config.config = yaml.safe_load(fh)
+
+    return read_config.config
 
 def download_image(url, path):
     """Downloads an image from an URL to the specified path."""
