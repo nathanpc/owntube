c70786efa837f3e79bd4034245ab8b3c7c1ddcad
Nathan Campos <nathan@innoveworkshop.com>
2023-09-07 16:22:01 +0100

    Created a bare minimum database for our service
    Created a very simple database that only contains the information we
    currently need to import a subscription dump into it.

---
+++ sql/initialize.sql
@@ -0,0 +1,32 @@
+-- initialize.sql
+-- Initializes the database creating the basic tables for our project.
+--
+-- Author: Nathan Campos <nathan@innoveworkshop.com>
+
+CREATE DATABASE IF NOT EXISTS owntube
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+
+USE owntube;
+
+CREATE TABLE channels(
+	cid			VARCHAR(30)		CHARACTER SET 'ascii' COLLATE 'ascii_bin' NOT NULL PRIMARY KEY,
+	name		VARCHAR(100)	NOT NULL UNIQUE,
+	description	TEXT			NULL
+);
+
+CREATE TABLE videos(
+	vid				VARCHAR(11)		CHARACTER SET 'ascii' COLLATE 'ascii_bin' NOT NULL PRIMARY KEY,
+	channel_cid		VARCHAR(30)		CHARACTER SET 'ascii' COLLATE 'ascii_bin' NOT NULL,
+	title			VARCHAR(100)	NOT NULL,
+	description		TEXT			DEFAULT "",
+	published_date	DATETIME		NOT NULL,
+	duration		INT(11)			NULL,
+
+	INDEX (title),
+	FULLTEXT KEY (description),
+
+	FOREIGN KEY (channel_cid)
+		REFERENCES channels (cid)
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
