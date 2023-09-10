27acfc073427f5ed832220a0a65a5969349a5d5d
Nathan Campos <nathan@innoveworkshop.com>
2023-09-10 12:15:36 +0100

    Made exceptions serializable
    In order to easily render exceptions in the web service a base exception
    class was created that automatically brings serialization to its
    parents.

--- owntube/exceptions.py
+++ owntube/exceptions.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 """A collection of the exceptions found throughout the project."""
 
-class ChannelNotFound(Exception):
+class OwnTubeBaseException(Exception):
+    def __dict__(self):
+        return {
+            'type': self.__class__.__name__,
+            'message': str(self),
+            'args': self.args
+        }
+
+class ChannelNotFound(OwnTubeBaseException):
     def __init__(self, message = "Channel wasn't found in the database"):
         super().__init__(message)
 
-class VideoNotFound(Exception):
+class VideoNotFound(OwnTubeBaseException):
     def __init__(self, message = "Video wasn't found in the database"):
         super().__init__(message)
 
-class SubscriptionFeedFetchError(Exception):
+class SubscriptionFeedFetchError(OwnTubeBaseException):
     def __init__(self, message = "An error occurred while fetching the subscriptions"):
         super().__init__(message)
 
-class VideoDownloadError(Exception):
+class VideoDownloadError(OwnTubeBaseException):
     def __init__(self, message = "An error occurred while downloading the video", data = None):
         super().__init__(message)
         self.data = data
